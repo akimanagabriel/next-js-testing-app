763f1c8fa0723708fafd738898079fa8ecc11424
akimanagabriel <gabsonbill@yahoo.com>
2024-03-12 13:36:52 +0200

    first adding

---
+++ app/page.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function HomePage() {
+  return (
+    <div className='my-12 container px-10 mx-auto'>
+      <h1 className='text-center text-5xl text-indigo-900'>Welcome</h1>
+      <p className='mt-10'>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Obcaecati id temporibus eos ea repellendus consequatur? Quo accusamus earum soluta atque.
+      </p>
+    </div>
+  )
+}
+
+export default HomePage
